7c442eeb5a9dfb11de942e6b60510c7de03fc9bb
Arman Abbasian <abasian.arman@gmail.com>
2023-11-26 15:53:02 +0330

    make static SSG and dynamic SSG in next 13

--- app/blogs/[blog]/page.tsx
+++ app/blogs/[blog]/page.tsx
@@ -1,15 +1,43 @@
-'use client'
-
 import Link from "next/link"
+import axios from 'axios';
+import { IBlog } from "../page";
 
-function Blog(props:any) {
 
-  console.log(props)
-    return (
-      <div>
-        blog
-      </div>
-    )
+async function getBlogs(id){
+  try {
+    const {data}= await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    return data
+  } catch (error) {
+    console.log(error)
   }
-  
-  export default Blog
+}
+async function Blogs({params}) {
+ 
+const id=params.blog;
+const blog:IBlog|undefined=await getBlogs(id)
+  return (
+    <div>
+      <h1>Blog</h1>
+        {blog && 
+        <div>
+          <h1>{blog.title}</h1>
+          <p>{blog.body}</p>
+        </div>
+        }
+    </div>
+  )
+}
+
+export default Blogs
+
+
+export async function generateStaticParams() {
+  try {
+    const {data}= await axios.get("https://jsonplaceholder.typicode.com/posts")
+   return data.map((item:IBlog)=>{
+       {id:String(item.id)}
+    })
+  } catch (error) {
+    console.log(error)
+  }
+} 
--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,12 +1,35 @@
 import Link from "next/link"
+import axios from 'axios';
 
-function Blogs() {
+export interface IBlog {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+async function getBlogs(){
+  try {
+    const {data}= await axios.get("https://jsonplaceholder.typicode.com/posts")
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+async function Blogs() {
+  console.log(await getBlogs())
+ const blogList:IBlog[]= await getBlogs()
+
   return (
     <div>
       <h1>Blogs</h1>
-        <Link href={'/blogs/1'}>blog 1</Link>
-        <Link href={'/blogs/2'}>blog 2</Link>
-        <Link href={'/blogs/3'} replace >blog 3</Link>
+        {
+        blogList.map((item:IBlog)=>{
+      return <p key={item.id}>
+        <Link href={`/blogs/${String(item.id)}`}>{item.title}</Link>
+        </p>
+}
+        )}
     </div>
   )
 }
